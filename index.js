3172ef4481064ed2aca976f2cf2e2050e90add77
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2018-07-07 20:48:29 -0700

    Switches d3 version to v4 and makes dummy chart

--- index.js
+++ index.js
@@ -2,4 +2,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   console.log(c3);
   console.log(d3);
+  let chart = c3.generate({
+    bindto: "#graph",
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+  });
 });
